4d81b0793df675bfac8f9dfd9421668facc943a7
Rock Neurotiko <miguelglafuente@gmail.com>
2021-02-12 09:40:37 +0100

    echo -e to allow \n

--- entry.sh
+++ entry.sh
@@ -10,7 +10,7 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 # Replace
 sed -i -E 's/^\.\/valheim_server\.x86_64 .+$/\.\/valheim_server.x86_64 -name "${SERVER_NAME}" -port "${SERVER_PORT}" -world "${SERVER_WORLD}" -password "${SERVER_PASSWORD}" -public "${SERVER_PUBLIC}" -savedir "${SERVER_DATA_DIR}"/g' "${STEAMAPPDIR}/start_server.sh"
 
-echo "${STEAM_ADMIN_ID}" > "${SERVER_DATA_DIR}/adminlist.txt"
+echo -e "${STEAM_ADMIN_ID}" > "${SERVER_DATA_DIR}/adminlist.txt"
 
 cd "${STEAMAPPDIR}"
 
